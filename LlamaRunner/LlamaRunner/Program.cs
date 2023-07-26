ad97a5258fa1dc25955bc487102a7496419040e9
Connorblu <cablumsack@gmail.com>
2023-07-25 22:46:27 -0400

    Initial commit of project

---
+++ LlamaRunner/LlamaRunner/Program.cs
@@ -0,0 +1,8 @@
+﻿using LLama;
+
+var model = new LLamaModel(new LLamaParams(
+    model: Path.Combine("..", "..", "..", "Models", "wizardLM-7B.ggmlv3.q4_1.bin"),
+    n_ctx: 512,
+    interactive: true,
+    repeat_penalty: 1.0f,
+    verbose_prompt: false));
