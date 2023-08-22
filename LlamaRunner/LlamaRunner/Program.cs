61f22edd08125544d8fb8dbad8d4753604bb6f69
Connorblu <cablumsack@gmail.com>
2023-08-21 23:04:25 -0400

    Outlining Frontend

--- LlamaRunner/LlamaRunner/Program.cs
+++ LlamaRunner/LlamaRunner/Program.cs
@@ -6,3 +6,21 @@ var model = new LLamaModel(new LLamaParams(
     interactive: true,
     repeat_penalty: 1.0f,
     verbose_prompt: false));
+
+var session = new ChatSession<LLamaModel>(model)
+    .WithPrompt("")
+    .WithAntiprompt(new[] { "User: " });
+
+Console.WriteLine();
+Console.Write("User: ");
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    var prompt = Console.ReadLine() + "\n";
+
+    Console.ForegroundColor = ConsoleColor.White;
+    foreach (var output in session.Chat(prompt, encoding: "UTF-8"))
+    {
+        Console.Write(output);
+    }
+}
